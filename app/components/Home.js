d736bcbfef5121575a4ead775d33705b1dfde05a
Carlos Perez <solracias@gmail.com>
2017-09-10 21:54:10 -0700

    added project area now. need to cut down accordian

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,12 +1,18 @@
 var React = require("react");
 import { HashRouter, Route, Switch } from 'react-router-dom';
 
+// import components
+import Project1 from "./child1/Project1";
+import Project2 from "./child1/Project2";
+
+
 export default class Home extends React.Component {
     
     render(){
         return(
             <div>
-                
+                <Project1 />
+                <Project2 />
             </div>
         )
     }
---
+++ app/components/child1/Project2.js
@@ -0,0 +1,14 @@
+var React = require("react");
+import { HashRouter, Route, Switch } from 'react-router-dom';
+
+export default class Project2 extends React.Component {
+
+
+    render(){
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
