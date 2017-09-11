2b524820ac5adee19c4492b5f44a5fe1b0a779b6
Carlos Perez <solracias@gmail.com>
2017-09-10 19:30:04 -0700

    added navbar for test

---
+++ app/components/Navbar.js
@@ -0,0 +1,12 @@
+var React = require("react");
+import { HashRouter, Route, Switch } from 'react-router-dom';
+
+export default class Navbar extends React.Component {
+
+
+    render(){
+        return(
+            <h1>test</h1>
+        )
+    }
+}
--- app/config/routes.js
+++ app/config/routes.js
@@ -3,13 +3,15 @@ import { HashRouter, Route, Switch } from 'react-router-dom';
 
 // require Main.js
 import Home from "../components/Home";
+import Navbar from "../components/Navbar";
 
 module.exports = (
     <HashRouter > 
         <div>
-            <Switch>
-                <Route path="/" component={Home} /> 
-            </Switch>
+            <Navbar />
+                <Switch>
+                    <Route path="/" component={Home} /> 
+                </Switch>
         </div>
     </HashRouter>
 );
