6bde72d516b16c2beb6b42cfaffbfac28fd3269f
Carlos Perez <solracias@gmail.com>
2017-09-10 23:48:20 -0700

    shortened collapse component

--- app/components/child2/Collapse1.js
+++ app/components/child2/Collapse1.js
@@ -39,20 +39,6 @@ export default class Collapse1 extends React.Component {
                                 </div>
                                 </div>
                             </div>
-                            <div className="card">
-                                <div className="card-header" role="tab" id="headingThree">
-                                <h5 className="mb-0">
-                                    <a className="collapsed" data-toggle="collapse" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                                    Collapsible Group Item #3
-                                    </a>
-                                </h5>
-                                </div>
-                                <div id="collapseThree" className="collapse" role="tabpanel" aria-labelledby="headingThree" data-parent="#accordion">
-                                <div className="card-body">
-                                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-                                </div>
-                                </div>
-                            </div>
                         </div>
                     </div>
                 </div>
