fa3e0f9db9e81ad084560fb969bcafc9205cce9f
Carlos Perez <solracias@gmail.com>
2017-09-10 19:49:32 -0700

    added buttons to navbar

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -7,7 +7,16 @@ export default class Navbar extends React.Component {
     render(){
         return(
             <nav className="navbar navbar-dark bg-dark">
-                
+                <div class="collapse navbar-collapse" id="navbarText">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">Projects</a>
+                        </li>
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">About Me</a>
+                        </li>
+                    </ul>
+                </div>
             </nav>
         )
     }
