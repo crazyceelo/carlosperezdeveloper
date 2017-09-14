325c4ba48e4f805d11ba79accbb3b3d5a5b4e90c
Carlos Perez <solracias@gmail.com>
2017-09-13 22:35:40 -0700

    Project and about me sections are set up.

--- app/components/child1/Project1.js
+++ app/components/child1/Project1.js
@@ -3,30 +3,58 @@ import { HashRouter, Route, Switch } from 'react-router-dom';
 
 // import child 2
 import Collapse1 from "../child2/Collapse1";
-import Collapse2 from "../child2/Collapse2";
 
 export default class Project1 extends React.Component {
 
 
     render(){
         return(
-            <div>
+            <div id="accordion" role="tablist">
                 <br />
-                <div className="row justify-content-start">
-                    <p  className="col-3">
-                        <button className="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-                            Project 1
-                        </button>
-                    </p>
-                    <p className="col-3">
-                        <button className="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample2" aria-expanded="false" aria-controls="collapseExample">
-                            Project 2
-                        </button>
-                    </p>
-                    <Collapse1 />
-                    <Collapse2 />
+                <div className="card">
+                    <div className="card-header" role="tab" id="headingOne">
+                    <h5 className="mb-0">
+                        <a data-toggle="collapse" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                        Cook Book
+                        </a>
+                    </h5>
+                    </div>
+
+                    <div id="collapseOne" className="collapse show" role="tabpanel" aria-labelledby="headingOne" data-parent="#accordion">
+                    <div className="card-body">
+                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                    </div>
+                    </div>
                 </div>
-            </div>
+                <div className="card">
+                    <div className="card-header" role="tab" id="headingTwo">
+                    <h5 className="mb-0">
+                        <a className="collapsed" data-toggle="collapse" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                        Spot Me
+                        </a>
+                    </h5>
+                    </div>
+                    <div id="collapseTwo" className="collapse" role="tabpanel" aria-labelledby="headingTwo" data-parent="#accordion">
+                    <div className="card-body">
+                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                    </div>
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="card-header" role="tab" id="headingThree">
+                    <h5 className="mb-0">
+                        <a className="collapsed" data-toggle="collapse" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                        Collapsible Group Item #3
+                        </a>
+                    </h5>
+                    </div>
+                    <div id="collapseThree" className="collapse" role="tabpanel" aria-labelledby="headingThree" data-parent="#accordion">
+                    <div className="card-body">
+                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                    </div>
+                    </div>
+                </div>
+                </div>
                 
         )
     }
