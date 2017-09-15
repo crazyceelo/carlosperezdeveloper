8d95dcfee23c5bc263a5a6ada567bb16e2636d37
Carlos Perez <solracias@gmail.com>
2017-09-14 20:49:34 -0700

    background layout is done

--- app/components/child1/Project1.js
+++ app/components/child1/Project1.js
@@ -22,20 +22,34 @@ export default class Project1 extends React.Component {
 
                     
                     <div id="collapseOne" className="collapse show" role="tabpanel" aria-labelledby="headingOne" data-parent="#accordion">
-                        <div className="card-body">
-                        <h5 className="card-body text-center">title</h5>
-                            <p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry 
-                                richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard 
-                                dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf 
-                                moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla 
-                                assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore 
-                                wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher 
-                                vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic 
-                                synth nesciunt you probably haven't heard of them accusamus labore 
-                                sustainable VHS.
-                            </p>
-                            <h5 className="card-body text-center">title</h5>
-                            asdfasdfasdfasdaf asdf asdfasdf 
+                        <div className="card-body cookBookBackground">
+                            <div className="border border-light backgroundColor">
+                                <h5 className="card-body text-center">Link</h5>
+                                <div className="text-center">
+                                    <a href="#/projects"> Click here to go to the web site</a>
+                                </div>
+                            </div>
+                            <br />
+                            <div className="border backgroundColor">
+                                <h5 className="card-body text-center">Description</h5>
+                                <div className="row">
+                                    <p className="card-body">Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry 
+                                        richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard 
+                                        dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf 
+                                        moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla 
+                                        assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore 
+                                        wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher 
+                                        vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic 
+                                        synth nesciunt you probably haven't heard of them accusamus labore 
+                                        sustainable VHS.
+                                    </p>
+                                </div>
+                            </div>
+                            <br />
+                            <div className="border backgroundColor">
+                                <h5 className="card-body text-center">Technology</h5>
+                                <p className="card-body">asdfasdfasdfasdaf asdf asdfasdf </p>
+                            </div>
                         </div>
                         
                     </div>
