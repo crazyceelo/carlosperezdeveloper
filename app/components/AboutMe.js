5e9c57959874a838cd594154615243ef53bd41f2
Carlos Perez <solracias@gmail.com>
2017-09-16 00:02:23 -0700

    added tech skills content

--- app/components/AboutMe.js
+++ app/components/AboutMe.js
@@ -24,7 +24,7 @@ export default class AboutMe extends React.Component {
                   <h4>Introduction</h4>
                   <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
                   <h4>Tech Skills</h4>
-                  <p>Donec sed odio dui. Nullam quis risus eget urna mollis ornare vel eu leo. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+                  <p>React, HTML, CSS, Javascript, C#, Node.js, npm, MySQL, Jquery, RESTful API(AJAX), Express, Handlebars, MongoDB</p>
                 </div>
               </div>
                 <div className="card-footer text-muted">
