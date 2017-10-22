dcf32b2fd1bd735a957fbacb1b54ef76d4434881
Carlos Perez <solracias@gmail.com>
2017-10-22 11:57:14 -0700

    made text white and made a send button

--- app/components/child2/ContactForm.js
+++ app/components/child2/ContactForm.js
@@ -10,9 +10,9 @@ export default class ContactForm extends React.Component {
             <section className="section">
             
                 {/* <!--Section heading--> */}
-                <h2 className="section-heading h1 pt-4">Contact us</h2>
+                <h2 className="section-heading h1 pt-4 whiteText">Contact me</h2>
                 {/* <!--Section description--> */}
-                <p className="section-description">Do you have any questions? Please do not hesitate to contact us directly. Our team will come back to you within matter of hours to help you.</p>
+                <p className="section-description whiteText">Feel free to ask my any questions.</p>
             
                 <div className="row">
             
@@ -28,7 +28,7 @@ export default class ContactForm extends React.Component {
                                     <div className="md-form">
                                         <div className="md-form">
                                             <input type="text" id="name" name="name" className="form-control" />
-                                            <label htmlFor="name" className="">Your name</label>
+                                            <label htmlFor="name" className="whiteText">Your name</label>
                                         </div>
                                     </div>
                                 </div>
@@ -39,7 +39,7 @@ export default class ContactForm extends React.Component {
                                     <div className="md-form">
                                         <div className="md-form">
                                             <input type="text" id="email" name="email" className="form-control" />
-                                            <label htmlFor="email" className="">Your email</label>
+                                            <label htmlFor="email" className="whiteText">Your email</label>
                                         </div>
                                     </div>
                                 </div>
@@ -53,7 +53,7 @@ export default class ContactForm extends React.Component {
                                 <div className="col-md-12">
                                     <div className="md-form">
                                         <input type="text" id="subject" name="subject" className="form-control" />
-                                        <label htmlFor="subject" className="">Subject</label>
+                                        <label htmlFor="subject" className="whiteText">Subject</label>
                                     </div>
                                 </div>
                             </div>
@@ -66,8 +66,10 @@ export default class ContactForm extends React.Component {
                                 <div className="col-md-12">
             
                                     <div className="md-form">
-                                        <textarea type="text" id="message" name="message" className="md-textarea"></textarea>
-                                        <label htmlFor="message">Your message</label>
+                                        <textarea type="text" id="message" name="message" className="form-control" rows="5"></textarea>
+                                        <div>
+                                            <label htmlFor="message" className="whiteText">Your message</label>
+                                        </div>
                                     </div>
             
                                 </div>
@@ -77,7 +79,7 @@ export default class ContactForm extends React.Component {
                         </form>
             
                         <div className="center-on-small-only">
-                            <a className="btn btn-primary" onClick="document.getElementById('contact-form').submit();">Send</a>
+                            <button className="btn btn-primary" onClick="document.getElementById('contact-form').submit();">Send</button>
                         </div>
                         <div className="status" id="status"></div>
                     </div>
