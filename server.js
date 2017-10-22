22a98793f78dc6ceacb4449bbb8439567f9b2ed0
Carlos Perez <solracias@gmail.com>
2017-10-22 12:43:04 -0700

    fixed an error in the mailOptions

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var logger = require("morgan");
 var mongoose = require("mongoose");
 var routes = require("./config/api-routes");
 var http = require ('http');
+const nodemailer = require('nodemailer');
 
 // Server configuration
 var app = express();
@@ -18,7 +19,44 @@ app.use(bodyParser.json({ type: "application/vnd.api+json"}));
 app.use(express.static("public"));
 
 // nodemailer configuration settings
-    // code
+// Generate test SMTP service account from ethereal.email
+nodemailer.createTestAccount((err, account) => {
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+        host: 'smtp.ethereal.email',
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: account.user, // generate ethereal user
+            pass: account.pass // generated ethereal password
+        }
+    });
+
+    // setup email data with unicode symbols
+    let mailOptions = {
+        from: '"test tester" <test@tester.com>', // sender address
+        to: 'solracias@gmail.com', // list of receivers. use comma to separate
+        subject: 'Hello', // subject line
+        text: 'Hello world!', // plain text body
+        html: '<b>Hellow world!</b>' // html body
+    };
+
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error){
+            return console.log(error);
+        }
+        else {
+            console.log('Message sent: %s', info.messageId);
+            
+            // preview only available when sending through an Ethereal account
+            console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+            // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@blurdybloop.com>
+            // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+        }
+    });
+});
 
 // MongoDB configuration
 // mongoose.connect("mongodb://localhost/personal-trainer-app");
